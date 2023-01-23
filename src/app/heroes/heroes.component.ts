ff3404191349d357e2cf408ae47906074f51c371
lcluischavez <lcluischavezz@gmail.com>
2023-01-23 02:20:53 -0600

    Message and Message Service completed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -8,14 +9,17 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 
-export class HeroesComponent {
-  heroes: Hero[] = [];
+export class HeroesComponent implements OnInit {
+
   selectedHero?: Hero;
 
-  constructor(private heroService: HeroService) {}
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) {}
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
   getHeroes(): void {
